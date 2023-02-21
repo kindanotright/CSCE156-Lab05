ea3c34233b403d1af6b04d9aad16bb0caa53d962
Chris Bourke <cbourke@cse.unl.edu>
2023-02-20 19:10:17 -0600

    Added documentation on the intent/expectations.

--- src/main/java/com/cinco/payroll/Employee.java
+++ src/main/java/com/cinco/payroll/Employee.java
@@ -2,4 +2,15 @@ package com.cinco.payroll;
 
 public class Employee {
 
+	/**
+	 * Returns a string representation (for printing) of the employee.
+	 * For example, for a {@link SalaryEmployee} this method should return 
+	 * <code>"Salary"</code>.
+	 * 
+	 * This method has been commented out to ensure the initial project
+	 * compiles.  Uncomment it once you make this class abstract.
+	 * 
+	 * @return
+	 */
+	//public abstract String getType();
 }
