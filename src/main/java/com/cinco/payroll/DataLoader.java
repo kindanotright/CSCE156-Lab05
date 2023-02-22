d94911a292896f8ea69a3f8f632de90f4a029019
Chris Bourke <cbourke@cse.unl.edu>
2023-02-22 06:11:57 -0600

    Fix index issue; more verbose error message

--- src/main/java/com/cinco/payroll/DataLoader.java
+++ src/main/java/com/cinco/payroll/DataLoader.java
@@ -12,13 +12,15 @@ public class DataLoader {
 	public static List<Employee> loadEmployees() {
 
 		List<Employee> result = new ArrayList<Employee>();
+		
+		String line = null;
 
 		try (Scanner s = new Scanner(new File(FILE_PATH))) {
 
 			int numRecords = Integer.parseInt(s.nextLine());
 			for (int i = 0; i < numRecords; i++) {
 
-				String line = s.nextLine();
+				line = s.nextLine();
 				if (!line.trim().isEmpty()) {
 					Employee e = null;
 					String tokens[] = line.split(",");
@@ -27,10 +29,10 @@ public class DataLoader {
 					String firstName = tokens[3];
 					String title = tokens[4];
 					double annualSalary = 0.0, hourlyPayRate = 0.0, hoursWorked = 0.0;
-					if (tokens.length == 6) {
+					if (tokens.length == 7) {
 						hourlyPayRate = Double.parseDouble(tokens[5]);
 						hoursWorked = Double.parseDouble(tokens[6]);
-					} else if (tokens.length == 5) {
+					} else if (tokens.length == 6) {
 						annualSalary = Double.parseDouble(tokens[5]);
 					}
 
@@ -49,7 +51,7 @@ public class DataLoader {
 				}
 			}
 		} catch (Exception e) {
-			throw new RuntimeException(e);
+			throw new RuntimeException("Encountered Error on line " + line, e);
 		}
 
 		return result;
