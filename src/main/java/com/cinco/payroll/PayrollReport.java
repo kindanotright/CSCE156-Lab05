7d247249e34a82db047f1f2c7d37bf9b0c66fb30
Chris Bourke <cbourke@cse.unl.edu>
2024-02-20 15:35:35 -0600

    Removed reference to "type" from the report

--- src/main/java/com/cinco/payroll/PayrollReport.java
+++ src/main/java/com/cinco/payroll/PayrollReport.java
@@ -11,19 +11,18 @@ public class PayrollReport {
 	
 	public static void main(String args[]) {
 
-		//get the employees from the data file
 		List<Employee> payroll = DataLoader.loadEmployees();
 
 		StringBuilder sb = new StringBuilder();
 		
-		sb.append(String.format("%-8s %-20s %-10s %-30s %9s %9s %9s\n", 
-				"ID", "Name", "Type", "Title", "Gross", "Taxes", "Net"));
+		sb.append(String.format("%-8s %-20s %-30s %9s %9s %9s\n", 
+				"ID", "Name", "Title", "Gross", "Taxes", "Net"));
 
 		//for each employee
 		for(Employee e : payroll) {
 			//format their information
-			sb.append(String.format("%-8s %-20s %-10s %-30s $%8.2f $%8.2f $%8.2f\n", 
-					"TODO", "TODO", "TODO", "TODO", 0.0, 0.0, 0.0)); //TODO: replace these
+			sb.append(String.format("%-8s %-20s %-30s $%8.2f $%8.2f $%8.2f\n", 
+					"TODO", "TODO", "TODO", 0.0, 0.0, 0.0)); //TODO: replace these with the appropriate method calls
 		}
 		
 		System.out.println(sb);
